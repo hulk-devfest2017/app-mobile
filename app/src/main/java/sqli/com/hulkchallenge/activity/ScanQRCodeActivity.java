a19ad02bd418bd7a2decced4efd47a9f34e8b987
renaud cadel <renaud.cadel@gmail.com>
2017-10-17 17:56:47 +0200

    changement de lib de scan + gestion des deux format de qrcode

--- app/src/main/java/sqli/com/hulkchallenge/activity/ScanQRCodeActivity.java
+++ app/src/main/java/sqli/com/hulkchallenge/activity/ScanQRCodeActivity.java
@@ -6,21 +6,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
-import com.google.zxing.Result;
-
+import ezvcard.Ezvcard;
+import ezvcard.VCard;
 import it.auron.library.mecard.MeCard;
 import it.auron.library.mecard.MeCardParser;
-import me.dm7.barcodescanner.zxing.ZXingScannerView;
+import me.dm7.barcodescanner.zbar.Result;
+import me.dm7.barcodescanner.zbar.ZBarScannerView;
 import sqli.com.hulkchallenge.R;
 import sqli.com.hulkchallenge.factory.PlayerFactory;
+import sqli.com.hulkchallenge.model.Player;
 
-public class ScanQRCodeActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
-    private ZXingScannerView mScannerView;
+public class ScanQRCodeActivity extends AppCompatActivity implements ZBarScannerView.ResultHandler {
+    private ZBarScannerView mScannerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mScannerView = new ZXingScannerView(this);
+        mScannerView = new ZBarScannerView(this);
         setContentView(R.layout.activity_scan_qrcode);
         LinearLayout view = (LinearLayout)findViewById(R.id.camera);
         view.addView(mScannerView);
@@ -41,15 +43,23 @@ public class ScanQRCodeActivity extends AppCompatActivity implements ZXingScanne
 
     @Override
     public void handleResult(Result rawResult) {
-        MeCard meCard = null;
+        Player player = new Player();
         try {
-            String meCardString = rawResult.getText();
-             meCard = MeCardParser.parse(meCardString);
+            String rawResultText = rawResult.getContents();
+            if(rawResultText != null){
+                if (rawResultText.startsWith("BEGIN:MECARD")){
+                    MeCard meCard = MeCardParser.parse(rawResultText);
+                    player = PlayerFactory.getPlayer(meCard);
+                } else if (rawResultText.startsWith("BEGIN:VCARD")){
+                    VCard vCard = Ezvcard.parse(rawResultText).first();
+                    player = PlayerFactory.getPlayer(vCard);
+                }
+            }
         } catch (Exception e) {
             Toast.makeText(this, "Erreur lors de la récupération du qrcode", Toast.LENGTH_SHORT).show();
         }
         Intent intent = new Intent(this, AddPlayerActivity.class);
-        intent.putExtra(AddPlayerActivity.PLAYER_INFORMATION, PlayerFactory.getPlayer(meCard));
+        intent.putExtra(AddPlayerActivity.PLAYER_INFORMATION, player);
         startActivity(intent);
     }
 }
