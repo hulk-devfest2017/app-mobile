aa79256ede3dfa5ef1cd59c0baebc976c0406a1d
renaud cadel <renaud.cadel@gmail.com>
2017-10-17 15:27:10 +0200

    ajout de la gestion du genre

--- app/src/main/java/sqli/com/hulkchallenge/activity/AddPlayerActivity.java
+++ app/src/main/java/sqli/com/hulkchallenge/activity/AddPlayerActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 
 import sqli.com.hulkchallenge.R;
 import sqli.com.hulkchallenge.model.Player;
@@ -15,34 +16,43 @@ public class AddPlayerActivity extends AppCompatActivity {
     public static final int REQUEST_CODE = 12;
     public static String PLAYER_INFORMATION = "PLAYER_INFORMATION";
     private Player playerInformation;
+    private RadioButton female;
+    private RadioButton male;
+    private EditText twitter;
+    private EditText company;
+    private EditText email;
+    private EditText lastName;
+    private EditText firstName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_player);
 
-        EditText firstName = (EditText) findViewById(R.id.firstName);
-        EditText lastName = (EditText) findViewById(R.id.lastName);
-        EditText email = (EditText) findViewById(R.id.email);
-        EditText company = (EditText) findViewById(R.id.company);
-        EditText twitter = (EditText) findViewById(R.id.twitter);
+        firstName = (EditText) findViewById(R.id.firstName);
+        lastName = (EditText) findViewById(R.id.lastName);
+        email = (EditText) findViewById(R.id.email);
+        company = (EditText) findViewById(R.id.company);
+        twitter = (EditText) findViewById(R.id.twitter);
+        male = (RadioButton) findViewById(R.id.male);
+        female = (RadioButton) findViewById(R.id.female);
 
         if(getIntent().getExtras() != null){
             playerInformation = (Player) getIntent().getExtras().getSerializable(PLAYER_INFORMATION);
-
-            if(playerInformation != null){
-                firstName.setText(playerInformation.getFirstName());
-                lastName.setText(playerInformation.getLastName());
-                email.setText(playerInformation.getEmail());
-                company.setText(playerInformation.getCompany());
-                twitter.setText(playerInformation.getTwitter());
-
-            }
-
         }
 
         if (playerInformation == null){
             playerInformation = new Player();
+        }
+        firstName.setText(playerInformation.getFirstName());
+        lastName.setText(playerInformation.getLastName());
+        email.setText(playerInformation.getEmail());
+        company.setText(playerInformation.getCompany());
+        twitter.setText(playerInformation.getTwitter());
+        if (playerInformation.isMale()) {
+            male.setChecked(true);
+        } else {
+            female.setChecked(true);
         }
 
         setupLaunchBtn();
@@ -65,16 +75,12 @@ public class AddPlayerActivity extends AppCompatActivity {
         launchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText firstName = (EditText) findViewById(R.id.firstName);
-                EditText lastName = (EditText) findViewById(R.id.lastName);
-                EditText email = (EditText) findViewById(R.id.email);
-                EditText company = (EditText) findViewById(R.id.company);
-                EditText twitter = (EditText) findViewById(R.id.twitter);
                 playerInformation.setCompany(company.getText().toString());
                 playerInformation.setEmail(email.getText().toString());
                 playerInformation.setLastName(lastName.getText().toString());
                 playerInformation.setFirstName(firstName.getText().toString());
                 playerInformation.setTwitter(twitter.getText().toString());
+                playerInformation.setMale(male.isChecked());
                 intent.putExtra(GameActivity.PLAYER_INFORMATION,playerInformation);
                 startActivityForResult(intent, REQUEST_CODE);
             }
